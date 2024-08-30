b94127b115fe4fa48ec23a5fd181b0e3fd3a8332
Vitor Costa Carvalho <49074487+VitorCCarvalho@users.noreply.github.com>
2024-08-30 17:56:46 -0300

    Added forum-page

---
+++ src/app/pages/forum-page/forum-page.component.ts
@@ -0,0 +1,46 @@
+import { FThread } from '../../components/fthread/fthread';
+import { Component, OnInit } from '@angular/core';
+import { FThreadService } from '../../components/fthread/fthread.service';
+import { ForumService } from '../../components/forum/forum.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Forum } from '../../components/forum/forum';
+
+
+@Component({
+  selector: 'app-forum-page',
+  templateUrl: './forum-page.component.html',
+  styleUrls: ['./forum-page.component.scss']
+})
+export class ForumPageComponent implements OnInit{
+  
+  listaFThreads : FThread[] = [];
+  forum!: Forum;
+
+  constructor(private fthreadService: FThreadService, 
+              private forumService: ForumService, 
+              private route: ActivatedRoute, 
+              private router: Router){}
+
+  ngOnInit(): void {
+    var forumId = this.route.snapshot.queryParamMap.get('forumId');
+    if(forumId){
+      console.log(forumId)
+      var numberForumId: number = +forumId
+      this.fthreadService.listarPorForum(numberForumId).subscribe((listafthreads) => {
+        this.listaFThreads = listafthreads
+      })
+      this.forumService.buscarPorId(numberForumId).subscribe((forum) => {
+        this.forum = forum
+      })
+    }
+    
+  }
+
+  verifySession(){
+    if(sessionStorage.getItem("jwt-session") != null){
+      this.router.navigate(['/newThread-page'], {queryParams: {forumId:this.forum.id ? this.forum.id : -1}})
+    }else {
+      // this.sidebar.openLoginDialog();
+    }
+  }
+}
