0cebc2d007e2ae88dfe55482b920e85ec535abdb
Vitor Costa Carvalho <49074487+VitorCCarvalho@users.noreply.github.com>
2024-08-30 17:56:30 -0300

    Added forum-page route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { ForumPageComponent } from './pages/forum-page/forum-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: AppComponent},
+    { path: 'forum-page', component: ForumPageComponent},
+];
