960bd4590e62b2f857992d7fc6e49cf7bc681f8b
Vitor Costa Carvalho <49074487+VitorCCarvalho@users.noreply.github.com>
2024-09-10 21:44:09 -0300

    fix: removed ang-mat and styling changes

--- src/app/components/fthread/fthread.component.ts
+++ src/app/components/fthread/fthread.component.ts
@@ -22,7 +22,7 @@ export class FthreadComponent implements OnInit{
               private forumService: ForumService,
               private fthreadReactionService: FthreadReactionService){}
 
-  @Input() id : number = 0
+  @Input() id : number = -1
   fthread : FThread = {
     "id": 0,
     "forumID": 6,
@@ -44,31 +44,33 @@ export class FthreadComponent implements OnInit{
   scoreResponse : string = ""
 
   ngOnInit(): void {
-    this.fthreadService.buscarPorId(this.id).subscribe((fthread) =>{
-      this.fthread = fthread;
-
-      if(this.fthread.userId){
-        this.userService.buscarPorId(this.fthread.userId).subscribe((user) => {
-          this.name = user.name
-        })
-        this.fthreadReactionService.buscarScore(this.id).subscribe((score) => {
-          this.fthread.score = score
-        })
+    if(this.id !== -1){
+      this.fthreadService.buscarPorId(this.id).subscribe((fthread) =>{
+        this.fthread = fthread;
   
-      }
-
-      if(this.fthread.forumID){
-        this.forumService.buscarPorId(this.fthread.forumID).subscribe((forum) => {
-          this.forum = forum.name
-        })
+        if(this.fthread.userId){
+          this.userService.buscarPorId(this.fthread.userId).subscribe((user) => {
+            this.name = user.name
+          })
+          this.fthreadReactionService.buscarScore(this.id).subscribe((score) => {
+            this.fthread.score = score
+          })
+    
+        }
   
-      }
-    });
+        if(this.fthread.forumID){
+          this.forumService.buscarPorId(this.fthread.forumID).subscribe((forum) => {
+            this.forum = forum.name
+          })
+    
+        }
+      });
+    }
+    
   }
 
   receiveScore(score: any){
     this.fthread.score = score 
-    console.log(this.fthread.score)
   }
 
 }
