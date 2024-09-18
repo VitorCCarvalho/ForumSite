409b58ed559540f332c590e646dafffa951dd77a
Vitor Costa Carvalho <49074487+VitorCCarvalho@users.noreply.github.com>
2024-09-18 11:48:39 -0300

    feat: added modal

---
+++ src/app/components/modal/modal.component.ts
@@ -0,0 +1,38 @@
+import { trigger, transition, style, animate } from '@angular/animations';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-modal',
+  standalone: true,
+  imports: [],
+  templateUrl: './modal.component.html',
+  styleUrl: './modal.component.scss',
+  animations: [
+    trigger('overlay', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('250ms', style({ opacity: .5 })),
+      ]),
+      transition(':leave', [
+        animate('500ms', style({ opacity: 0 }))
+      ])
+    ]),
+    
+    trigger('modal', [
+      transition(':enter', [
+        style({ top: -999 }),
+        animate('500ms', style({ top: '50%' })),
+      ]),
+      transition(':leave', [
+        animate('250ms', style({ top: -999 }))
+      ])
+    ]),
+  ]
+})
+export class ModalComponent {
+  show: boolean = false
+
+  toggle(){
+    this.show = !this.show;
+  }
+}
