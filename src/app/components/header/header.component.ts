2f5df9e4e76e7e1d81f77d8b149002fc03ca5f86
Vitor Costa Carvalho <49074487+VitorCCarvalho@users.noreply.github.com>
2024-09-18 21:33:38 -0300

    fix: fixed modal toggle

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { LoginDialogComponent } from "../dialog/login-dialog/login-dialog.component";
+import { ModalComponent } from '../modal/modal.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, LoginDialogComponent, ModalComponent],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
