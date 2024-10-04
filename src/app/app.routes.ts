8dff7163c21160dbaa47362b5583d50555dcc67e
Vitor Costa Carvalho <49074487+VitorCCarvalho@users.noreply.github.com>
2024-10-04 17:11:17 -0300

    feat: created new-thread-page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { ForumPageComponent } from './pages/forum-page/forum-page.component';
 import { HomeComponent } from './pages/home/home.component';
 import { FthreadPageComponent } from './pages/fthread-page/fthread-page.component';
+import { NewThreadPageComponent } from './pages/new-thread-page/new-thread-page.component';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
     { path: 'forum-page', component: ForumPageComponent},
-    { path: 'fthread-page', component: FthreadPageComponent}
+    { path: 'fthread-page', component: FthreadPageComponent},
+    { path: 'new-thread-page', component: NewThreadPageComponent}
 ];
---
+++ src/app/pages/new-thread-page/new-thread-page.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-thread-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './new-thread-page.component.html',
+  styleUrl: './new-thread-page.component.scss'
+})
+export class NewThreadPageComponent {
+
+
+  onSubmit(){}
+}
